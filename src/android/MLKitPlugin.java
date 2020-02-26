5134db446ca82d1529ccd92b7043bc112fde0d2b
Paul John Parreño <paul-john@newlogic.io>
2020-02-26 11:13:00 +0800

    commiting gitignore file

--- src/android/MLKitPlugin.java
+++ src/android/MLKitPlugin.java
@@ -1,5 +1,7 @@
 package com.impactlabs.cordova.plugin;
 // The native API
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 // Cordova-required packages
@@ -13,33 +15,40 @@ import org.json.JSONObject;
 import com.impactlabs.mlkitdemo.MLKitBaseActivity;
 
 public class MLKitPlugin extends CordovaPlugin {
-  
-  @Override
-  public boolean execute(String action, JSONArray args,
-    final CallbackContext callbackContext) {
-      // Verify that the user sent a 'show' action
-      if (!action.equals("startMLActivity")) {
-        callbackContext.error("\"" + action + "\" is not a recognized action.");
-        return false;
-      }
-      // String message;
-      // String duration;
-      // try {
-      //   JSONObject options = args.getJSONObject(0);
-      //   message = options.getString("message");
-      //   duration = options.getString("duration");
-      // } catch (JSONException e) {
-      //   callbackContext.error("Error encountered: " + e.getMessage());
-      //   return false;
-      // }
-      // Start calling the new activity
-      Context context=this.cordova.getActivity().getApplicationContext();
-      //or Context context=cordova.getActivity().getApplicationContext();
-      Intent intent=new Intent(context,MLKitBaseActivity.class);
-      context.startActivity(intent);
-      // Send a positive result to the callbackContext
-      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-      callbackContext.sendPluginResult(pluginResult);
-      return true;
-  }
+
+    private final int OP_MLKIT = 1001;
+
+    @Override
+    public boolean execute(String action, JSONArray args,
+                           final CallbackContext callbackContext) {
+        // Verify that the user sent a 'show' action
+        if (!action.equals("startMLActivity")) {
+            callbackContext.error("\"" + action + "\" is not a recognized action.");
+            return false;
+        }
+        // String message;
+        // String duration;
+        // try {
+        //   JSONObject options = args.getJSONObject(0);
+        //   message = options.getString("message");
+        //   duration = options.getString("duration");
+        // } catch (JSONException e) {
+        //   callbackContext.error("Error encountered: " + e.getMessage());
+        //   return false;
+        // }
+        // Start calling the new activity
+        Activity activity = this.cordova.getActivity();
+        //or Context context=cordova.getActivity().getApplicationContext();
+        Intent intent = new Intent(activity, MLKitBaseActivity.class);
+        activity.startActivityForResult(intent,OP_MLKIT);
+        // Send a positive result to the callbackContext
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+        callbackContext.sendPluginResult(pluginResult);
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
 }
